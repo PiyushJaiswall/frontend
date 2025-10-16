3ad040625dc7b08703b95f92f1415e1973713744
Piyush jaiswal <92733674+PiyushJaiswall@users.noreply.github.com>
2025-10-16 12:42:09 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -496,7 +496,7 @@ export default function Home() {
               )}
             </div>
           </div>
-        </div>
+
 
         {filteredMeetings.length > 0 ? (
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
